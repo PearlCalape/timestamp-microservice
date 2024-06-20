80f58c665e75f113cb2ed3a9dbeed08a68e371e4
PearlCalape <143477291+PearlCalape@users.noreply.github.com>
2024-06-20 23:28:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// index.js
+// server.js
 // where your node app starts
 
 // init project
@@ -8,70 +8,41 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+const url = require("url");
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
+
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+    res.json({ greeting: 'hello API' });
 });
 
-app.get("/api/:date?", (req,res) => {
-  let input = req.params.date;
+app.get("/api", (req, res) => {
+    const now = new Date();
+    res.json({ unix: now.getTime(), utc: now.toUTCString() })
+});
 
-  /*1.create variable for checking :  
-      a.isValidDate 
-        to check if input is string with valid date format -> 
-        if not valid date, it will return NaN 
-        example : 
-        -valid   : 2015-12-25, 
-        -invalid : 2015-02-31, 1451001600000
-  */
-  let isValidDate       = Date.parse(input); 
+app.get("/api/:date", (req, res) => {
+    const paramsDate = req.params.date;
+    const invalidDate = "Invalid Date";
+    const date = parseInt(paramsDate) < 10000
+        ? new Date(paramsDate)
+        : new Date(parseInt(paramsDate))
 
-  /*  b.isValidUnixNumber
-        to check if input is string with whole number(no symbol or character in the middle of input) -> 
-        it must be valid unix (source : https://benjaminsemah.com/build-timestamp-microservice)
-  */
-  let isValidUnixNumber = /^[0-9]+$/.test(input)
-
-  //  c.isEmpty to check there is nothing in input
-  let isEmpty = input == "" || input == null;
-  
-  //3.create another variables used in if-else
-  let unix_output = 0;
-  let utc_output  = "";
-  
-  if (isValidDate) {
-    unix_output = new Date(input);
-    utc_output  = unix_output.toUTCString();
-    // valueOf used for getting a variable back to primitive type
-    return res.json({unix : unix_output.valueOf(), utc : utc_output});
-  }
-  else if (isNaN(isValidDate) && isValidUnixNumber) {
-    unix_output = new Date(parseInt(input));
-    utc_output  = unix_output.toUTCString();
-    return res.json({unix : unix_output.valueOf(), utc : utc_output});
-  }
-  else if (isEmpty) {
-    unix_output = new Date();
-    utc_output  = unix_output.toUTCString();
-    return res.json({unix : unix_output.valueOf(), utc : utc_output});  
-  }
-  else {
-    res.json({error: "Invalid Date"});
-  }
-  
+    date.toString() === invalidDate
+        ? res.json({ error: invalidDate })
+        : res.json({ unix: date.valueOf(), utc: date.toUTCString() });
 });
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+    console.log('Your app is listening on port ' + listener.address().port);
 });
